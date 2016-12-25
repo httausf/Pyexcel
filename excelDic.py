c7874a6166efd40e1d30be369fb92fd24a314986
ProYan <yzh0829@foxmail.com>
2016-12-25 13:52:43 +0800

    Initial commit

---
+++ excelDic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2016/12/24 15:22
+# @Author  : ProY
+# @Site    : 
+# @File    : excelDic.py
+# @Software: PyCharm
+
+import xlrd,xlwt
+import os
+import sys
+
+class excel():
+	def __init__(self):
---
+++ sourse.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2016/12/24 15:22
+# @Author  : ProY
+# @Site    : 
+# @File    : sourse.py
+# @Software: PyCharm
